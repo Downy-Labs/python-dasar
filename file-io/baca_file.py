0d70171d968fb8bd851921b9efd932228b63ca6d
laludonisetiawan <laludonisetiawan@gmail.com>
2023-03-11 01:13:31 +0800

    membaca puisi readlines

---
+++ file-io/baca_file.py
@@ -0,0 +1,6 @@
+baca_file = open("puisi.txt", "r")
+puisi = baca_file.readlines()
+print(puisi[0])
+print(puisi[1])
+baca_file.close()
+
---
+++ read-write-file.py
@@ -0,0 +1,9 @@
+# belajar cara membaca dan menulis file di python
+
+"""
+mode baca pada python:
+ r = hanya baca saya
+ w = akses untuk menulis file, jika file sudah ada, maka file akan di replace dan diganti dengan yang baru ditulis
+ a = digunakan untuk append atau menambah data ke file, artinya jika sudah ada data dalam file, maka akan ditambahkan dan tidak direplace
+ r+ = digunakan untuk membaca sekaligus menulis data ke file
+"""
